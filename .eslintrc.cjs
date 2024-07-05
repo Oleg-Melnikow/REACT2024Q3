b02492f92ac55011d9ae02e8cf311bd98e51dbd1
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-07-05 22:23:31 +0300

    feat: project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json", "./tsconfig.app.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh", "prettier", "react-compiler"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react-compiler/react-compiler": "error",
+    "prettier/prettier": "error",
+    "no-debugger": "off",
+    "no-console": 0,
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-inferrable-types": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "react/no-array-index-key": "error",
+    "react/react-in-jsx-scope": "off",
+    "class-methods-use-this": "off",
+    "react/require-default-props": "error",
+    semi: ["error", "always"],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Component, ReactNode } from "react";
+import "./App.css";
+
+class App extends Component {
+  render(): ReactNode {
+    return <div>Component</div>;
+  }
+}
+
+export default App;
