dc7a01329b77a914274cf8b820c1626ce5225207
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-07-06 21:03:01 +0300

    feat: add logic for get items

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
-import { Component, ReactNode } from "react";
+import { PureComponent, ReactNode } from "react";
 import Search from "./components/Search/Search.tsx";
+import { ResultPeoople, People } from "./interfaces/interface";
+import { searchAPI } from "./api/api";
+import Loader from "./components/Loader/Loader.tsx";
 import "./App.css";
 
-class App extends Component {
+interface StateApp {
+  people: People[];
+  loading: boolean;
+}
+
+class App extends PureComponent {
+  state: StateApp = {
+    people: [],
+    loading: true,
+  };
+
+  async getItems(search?: string): Promise<void> {
+    this.setState({ loading: true });
+    try {
+      const result = search
+        ? await searchAPI.searchItems(search)
+        : await searchAPI.getItems();
+      const { results } = result.data as ResultPeoople;
+      this.setState({ people: results, loading: false });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async componentDidMount(): Promise<void> {
+    const item = localStorage.getItem("search") ?? "";
+    await this.getItems(item);
+  }
+
   render(): ReactNode {
+    const { loading } = this.state;
     return (
       <>
         <Search />
+        {loading && <Loader />}
       </>
     );
   }
---
+++ src/interfaces/interface.ts
@@ -0,0 +1,23 @@
+export interface ResultPeoople {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: People[];
+}
+
+export interface People {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  url: string;
+  vehicles: string[];
+  starships: string[];
+  species: string[];
+}
